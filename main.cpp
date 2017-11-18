a49a51e78e70ee65ea5b7b5b9e32d37e44bdb6be
Nick Ramirez <bcgnick@gmail.com>
2017-11-17 19:58:15 -0800

    finished nearest neighbor cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ const int INFINITE = 999999999;
 
 using namespace std;
 
+/*
+MST is actually probably not going to work for this problem the way it is
+because it can visit a node more than once. The traveling salesman states that
+you only visit a node one time. Look into Christofides Algorithm or a Dynamic
+Programming solution.
+*/
+
 class graph{
 public:
   graph(); //default constructor
@@ -14,8 +21,16 @@ public:
   void findNearestNeighbor(); //STILL NEED TO DO
   void findBruteForce(); //STILL NEED TO DO
 private:
-  int cities[SIZE][SIZE]; //adj matrix representation of graph. Holds weights
-  int city_names[SIZE]; //holds string names of city
+  //adj matrix representation of graph. Holds weights and is static
+  int cities[SIZE][SIZE] = {
+                            {0,5,7,1,8},
+                            {5,0,6,4,8},
+                            {7,6,0,2,7},
+                            {1,4,2,0,6},
+                            {8,8,7,6,0}
+                            };
+  //holds string names of city
+  string city_names[SIZE] = {"Los Angeles", "Anaheim", "San Francisco", "San Jose", "Sacramento"};
   void printMST(int mst[], int v); //used by findMST to print the MST
 
   /*returns the index of the minimum value for the vertices that
@@ -30,11 +45,14 @@ graph::graph(){
   //initilize diagonal across matrix
   for(int i = 0; i < SIZE; i++)
     cities[i][i] = 0;
-  };
 
+  }
+
+/*
 void graph::findNearestNeighbor(){
   int MST[SIZE];
 }
+*/
 
 void graph::printGraph(){
   //top row format
@@ -139,27 +157,79 @@ void graph::findMST(){
 	printMST(mst, SIZE);
 }
 
-graph::findNearestNeighbor(){
+void graph::findNearestNeighbor(){
   //need to fill in
+
+  bool visited[SIZE];
+  //holds city order
+  int order[SIZE+2];
+
+  //Initialize all to false
+  for (int i = 0; i < SIZE; i++){
+    visited[i] = false;
+  }
+  int min = INFINITE;
+
+  int min_index = 0;
+  int order_index = 0;
+  int temp_min_index;
+
+  //initialize
+  order[order_index++] = 0;
+  visited[0] = true;
+
+  for(int i = 0; i < SIZE; i++)
+  {
+    temp_min_index = min_index;
+    min = INFINITE;
+    for(int j = 0; j < SIZE; j++){
+      if ((cities[j][min_index] != 0) && (cities[j][min_index] < min) && (visited[j] == false)){
+        min = cities[j][min_index];
+        temp_min_index = j;
+      }
+    }
+
+    min_index = temp_min_index;
+    visited[min_index] = true;
+    order[order_index++] = min_index;
+  }
+
+  order[--order_index] = 0;
+
+  cout << "The order is: ";
+  for (int i = 0; i < SIZE + 1; i++){
+    cout << city_names[order[i]];
+    if (i < SIZE){
+      cout << " --> ";
+    }
+  }
+
+  cout << endl;
+
 }
 
-graph::findBruteForce(){
+void graph::findBruteForce(){
   //need to fill in
 }
 
 void printMenu(){
-  for(int i = 0; i < 40; i++){
-    cout << '-' << endl;
+  cout << setw(5);
+  for(int i = 0; i < 35; i++){
+    cout << '-';
   }
+  cout << endl << setw(5);
   cout << '|' << setw(17) << "MENU" << setw(17) << '|';
-  for(int i = 0; i < 40; i++){
-    cout << '-' << endl;
+  cout << endl << setw(5);
+  for(int i = 0; i < 35; i++){
+    cout << '-';
   }
-  cout << "\n\nWELCOME TO THE TRAVELING SALESMAN SOLUTION\n";
-  cout << "\n\nPlease choose from the following:\n";
-  cout << "1. Solve using Minimum Spanning Tree\n";
-  cout << "2. Solve using a Nearest Neighbor Algorithm\n";
-  cout << "3. Solve using Brute Force *NOT RECOMMENDED*\n";
+  cout << endl;
+  cout << "\n\n     WELCOME TO THE TRAVELING SALESMAN SOLUTION\n";
+  cout << "\n\n     Please choose from the following:\n";
+  cout << "     " << "1. Solve using Minimum Spanning Tree\n";
+  cout << "     " << "2. Solve using a Nearest Neighbor Algorithm\n";
+  cout << "     " << "3. Solve using Brute Force *NOT RECOMMENDED*\n";
+  cout << "     " << "4. Quit\n";
 }
 
 /*
@@ -170,31 +240,42 @@ int main(){
 
   graph G;
 
-  printMenu();
   int choice = -1;
   do{
+    printMenu();
     cin >> choice;
-    if (choice < 1 || choice > 3)
+    if (choice < 1 || choice > 4)
+    {
       cout << "INVALID CHOICE! PLEASE CHOOSE AGAIN\n";
-  }while(choice < 1 || choice > 3);
+    }
 
-  switch(choice){
-    case 1:{
-      break;
+    switch(choice){
+      case 1:
+      {
+        G.findMST();
+        break;
+      }
+      case 2:
+      {
+        G.findNearestNeighbor();
+        break;
+      }
+      case 3:
+      {
+        break;
+      }
+      case 4:
+      {
+        cout << "\nGoodbye!\n";
+        break;
+      }
+      default:
+      {
+        cout << "SORRY INVALID\n";
+      }
     }
-    case 2:
-    {
-      break;
-    }
-    case 3:
-    {
-      break;
-    }
-    default:
-    {
-      cout << "SORRY INVALID\n";
-    }
-  }
+
+  }while(choice > 0 && choice < 4);
 
   return 0;
 }
