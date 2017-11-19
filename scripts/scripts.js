a30ddcd8077e6c49471743982acaaf5780355b40
Nick Ramirez <bcgnick@gmail.com>
2017-11-19 15:38:42 -0800

    added in nearest neighbor JS. Still need to have to receive input from form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,3 +9,43 @@ var cities = [
               ];
 //holds string names of city
 var city_names = ["Los Angeles", "Anaheim", "San Francisco", "San Jose", "Sacramento"];
+
+
+function findNearestNeighbor(){
+  var visited[SIZE];
+  //holds city order
+  var order[SIZE+2];
+
+  //Initialize all to false
+  for (var i = 0; i < SIZE; i++){
+    visited.push(false);
+  }
+  var min = 99999999;
+
+  var min_index = 0;
+  var order_index = 0;
+  var temp_min_index;
+
+  //initialize
+  order[order_index++] = 0;
+  visited[0] = true;
+
+  for(int i = 0; i < SIZE; i++)
+  {
+    temp_min_index = min_index;
+    min = 999999999;
+    for(var j = 0; j < SIZE; j++){
+      if ((cities[j][min_index] != 0) && (cities[j][min_index] < min) && (visited[j] == false)){
+        min = cities[j][min_index];
+        temp_min_index = j;
+      }
+    }
+
+    min_index = temp_min_index;
+    visited[min_index] = true;
+    order[order_index++] = min_index;
+  }
+
+  order[--order_index] = 0;
+
+}
