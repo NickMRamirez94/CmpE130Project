bb86546f6111f2b01e77000ef52c591563ff0184
John Tumath <tumathjohn@gmail.com>
2017-11-22 19:23:46 -0800

    Expanded Brute force

--- scripts/brutescript.js
+++ scripts/brutescript.js
@@ -1,7 +1,8 @@
 
 
-var best_distance = MAX_SAFE_INTEGER,
-    best_path = [];
+var best_distance = 999999999,
+    best_path = new Array();
+    current_cities = new Array();
 
 var distance_map =[
                   [0,5,7,1,8],
@@ -11,21 +12,24 @@ var distance_map =[
                   [8,8,7,6,0]
                   ];
 
+var test_list = [1,2,3,4,5];
+var city_names = ["Los Angeles", "Anaheim", "San Francisco", "San Jose", "Sacramento"];
+
 function brute_total_distance(city_list)
 {
   var distance = distance_map[0][city_list[0]];
   for(var i = 0; i < city_list.length-1; i++)
   {
-    distance = distance + distance_map[city_list[i]][city_list[i+1]];
+    distance += distance_map[city_list[i]][city_list[i+1]];
   }
-  distance = distance + distance_map[city_list[city_list.length-1]][0];
+  distance += distance_map[city_list[city_list.length-1]][0];
   return distance;
 }
 
 function brute_force_path(best_path, best_distance, current_cities, remaining_cities)
 {
 
-    if(remaining_cities.length == 0)
+    if(remaining_cities.length === 0)
     {
       if(brute_total_distance(current_cities) < best_distance)
       {
@@ -45,4 +49,10 @@ function brute_force_path(best_path, best_distance, current_cities, remaining_ci
       }
     }
   }
+
+  brute_force_path(best_path, best_distance, 0, test_list);
+  console.log("Best Distance: ");
+  console.log(best_distance);
+  console.log("\nBest Path: ");
+  console.log(best_path);
 
